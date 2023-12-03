e55513d862c47223bb334bb2371440c9f248b2e5
Diaz <Diaz>
2023-12-03 17:11:51 +0100

    feat(back): ORM implementation done

--- FFBA-Diaz-app/app/Http/Controllers/FilmController.php
+++ FFBA-Diaz-app/app/Http/Controllers/FilmController.php
@@ -2,29 +2,80 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Http;
+use App\Models\Genre;
+use App\Models\Film;
 //use Illuminate\Http\Request;
 
 
 class FilmController extends Controller
 {
-    /*public function index()
-    {
-        $response = Http::get("https://api.themoviedb.org/3/movie/popular?api_key=da30e671936115ccc8bb59d618f91d8d");
-
-        $films = $response->json()['results'];
-
-        return view('films.index', compact('films'));
-    }*/
-
-
     
     public function index()
     {
-        $response = Http::get("https://api.themoviedb.org/3/movie/popular", [
+       //On recupere les donnés depuis l'API externe
+       $response = Http::get("https://api.themoviedb.org/3/movie/popular", [
+       'api_key' => config('services.tmdb.api_key')]);
+         
+       $films = $response->json()['results'];
 
-        'api_key' => config('services.tmdb.api_key')]);
+        
+       $response = Http::get("https://api.themoviedb.org/3/genre/movie/list", [
+       'api_key' => config('services.tmdb.api_key')]);
+            
+       $genres = $response->json()['genres'];
+                      
+      
+       //S'ils n'existent pas dans la BDD on les sauvegarde
 
-        $films = $response->json()['results'];
+       foreach($genres as $genre){    
+        $existingData = Genre::where('id', $genre['id'])
+                              ->where('name', $genre['name'])
+                              ->first();
+    
+        if (!$existingData) {
+            Genre::create([
+                'id' => $genre['id'],
+                'name' => $genre['name']     
+             ]
+             );
+        } 
+       }
+
+       foreach($films as $film){    
+         $existingData = Film::where('id', $film['id'])
+                               ->where('title', $film['title'])
+                               ->first();
+         if (!$existingData) {
+            
+              Film::create([
+                'title' => $film['title'],
+                'overview'=> $film['overview'],
+                'original_language'=> $film['original_language'],
+                'release_date'=> $film['release_date']
+             ]
+             );
+
+              $retrievedFilm = Film::where('title', $film['title'])->first();
+              $filmGenres = $film['genre_ids'];
+              $tableSize =  count($filmGenres);
+              
+              for($i = 0; $i<$tableSize; $i++){
+                 $genre = $retrievedFilm->genres()->attach([$filmGenres[$i]]);
+              }
+
+        
+         } 
+        }
+
+
+
+
+
+
+
+        //On recupere les données depuis la BDD
+
+        //On envois les données à la view
 
         return view('films.index', compact('films'));
     }
---
+++ FFBA-Diaz-app/app/Models/Film.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Film extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'overview',
+        'original_language',
+        'release_date'
+    ];
+
+    public function genres(){
+       return $this->belongsToMany(Genre::class);
+    }
+
+}
